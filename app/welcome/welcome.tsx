33ef6cd419b97de57c8edae81d5b132c667b9e8d
derynm <dery02.nm@gmail.com>
2025-02-24 22:58:44 +0700

    initail commit

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,10 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center min-h-dvh font-semibold text-3xl text-center">
+      Videoboxd
+    </main>
+  );
+}
