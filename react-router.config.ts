c5fa30224c8e22cb32620b46d071aefa9e668739
derynm <dery02.nm@gmail.com>
2025-02-26 10:13:38 +0700

    feat: disable SSR

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import { vercelPreset } from '@vercel/react-router/vite';
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: true,
+  ssr: false,
   presets: [vercelPreset()],
 } satisfies Config;
