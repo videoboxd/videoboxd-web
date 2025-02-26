0c833fbc9427ef82bac5df5aaf0b5c597c93ad6b
derynm <dery02.nm@gmail.com>
2025-02-26 10:14:45 +0700

    fix: remove vercel preset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,5 @@
-import { vercelPreset } from '@vercel/react-router/vite';
 import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: false,
-  presets: [vercelPreset()],
 } satisfies Config;
