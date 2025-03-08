612701db6ca210487311ca08dc14b9fea14b8830
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-03-08 13:49:14 +0800

    feat: create initial route for new video

---
+++ app/routes/new-video.tsx
@@ -0,0 +1,10 @@
+import type { Route } from "./+types/home";
+
+export default function NewVideo() {
+    return (
+      <div>
+        <h1 className="text-3xl">Create New Video</h1>
+      </div>
+    );
+  }
+  
