469dfaea75d1c683d234748d66dda941a70f4a10
Padillahirpan <padillahirpan8@gmail.com>
2025-03-11 22:23:53 +0700

    feat: add header

--- app/components/shared/navbar.tsx
+++ app/components/shared/navbar.tsx
@@ -9,17 +9,19 @@ type AuthUser = {
 
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 flex w-full items-center justify-between p-4 bg-neutral-950">
-      <Link to="/">
-        <h2 className="p-2 font-brand text-2xl tracking-tight text-white md:text-3xl">
-          Videoboxd
-        </h2>
-      </Link>
-      <div className="hidden items-center gap-8 text-base font-semibold md:flex">
-        <Link to="/login">
-          <div className="py-4 px-6 bg-blue-400 rounded-full">Sign In</div>
+    <div className="fixed flex w-full items-center justify-center p-4 bg-[#00000080]">
+      <div className="max-w-screen-xl w-full flex justify-between">
+        <Link to="/">
+          <h2 className="p-2 font-brand text-2xl tracking-tight text-white md:text-3xl">
+            Videoboxd
+          </h2>
         </Link>
+        <div className="hidden items-center gap-8 text-base font-semibold md:flex">
+          <Link to="/login">
+            <div className="py-4 px-6 bg-blue-400 rounded-full">Sign In</div>
+          </Link>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 }
