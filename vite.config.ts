2c2d434d3ba247402ad8daf1bd1c9100d3a592ec
derynm <dery02.nm@gmail.com>
2025-03-18 15:08:59 +0700

    feat: setup sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), sentryVitePlugin({
+    org: "videoboxd",
+    project: "videoboxd"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
