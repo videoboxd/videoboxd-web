db40cc79a2f2f5af5b5fe85a8ae8c383a466fa92
M Rifki Septiawan <rifkim91@gmail.com>
2025-03-28 01:35:16 +0700

    feat: add route and video details page based on identifier

--- app/routes.ts
+++ app/routes.ts
@@ -12,5 +12,6 @@ export default [
     route("/register", "routes/register.tsx"),
     route("/login", "routes/login.tsx"),
     route("/new", "routes/new-video.tsx"),
+    route("/videos/:identifier", "routes/video-details.tsx"),
   ]),
 ] satisfies RouteConfig;
