55b1dc58a258f977834beb22a099aedb462e7b25
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-06 10:50:48 +0700

    refactor: remove unused help section from footer component

--- app/components/shared/footer.tsx
+++ app/components/shared/footer.tsx
@@ -10,19 +10,6 @@ export function Footer() {
         </p>
         <div className="text-white">© Videoboxd. All Rights Reserved.</div>
       </div>
-      <div className="mt-6 flex flex-wrap justify-center gap-4 md:mt-0 md:flex-row md:gap-8">
-        <div className="min-w-[120px] flex-1 text-sm">
-          <h2 className="font-bold tracking-tight text-white">Help</h2>
-          <ul className="mt-2 space-y-1 text-white">
-            <li>
-              <Link to="/">Contact Us</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
     </footer>
   );
 }
